f8d75d013a993ef172f4cc38eb7b1e7f27d98c2c
Enrique <enrique.delgado@live.com>
2018-12-26 00:29:01 -0600

    Show pokemon name on card
    capitalize CSS title of every card

--- src/js/components/presentational/PokemonItem.jsx
+++ src/js/components/presentational/PokemonItem.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 
 class PokemonItem extends Component{
+    
+     
     render(){
+        const pokemonItem = this.props.pokemonItem;
         return(
             <div>
                 <div className="card c-card">
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
+                        <h5 className="c-card-title card-title">{pokemonItem.name}</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                        <a href="{pokemon.url}" className="btn btn-primary">Ver detalle...</a>
                     </div>
                 </div>
             </div>
