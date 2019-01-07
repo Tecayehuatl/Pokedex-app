fdf0cc69d5aae6348c54ef081484eaf9daad963b
Enrique <enrique.delgado@live.com>
2019-01-07 15:56:01 -0600

    remove link test

--- src/js/components/presentational/Main.jsx
+++ src/js/components/presentational/Main.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 //components
 import Pokemons from './Pokemons.jsx'
 import PokemonCard from './PokemonCard.jsx'
@@ -11,15 +11,12 @@ class Main extends Component {
             <main className="container o-main">
               <div>
                 <Router>
-                    <div>
-                        <NavLink to="pokemon-card" params={{name: 'enrique'}}>PC</NavLink>
-                        <Switch>
-                            {/*  Routes registered, we can use them where we want */}
-                            <Route path="/" component={ Pokemons } exact/>
-                            <Route path="/pokemon-card" component={ PokemonCard }/>
-                            <Route component={ ErrorPage } />
-                        </Switch>
-                   </div>
+                    <Switch>
+                        {/*  Routes registered, we can use them where we want */}
+                        <Route path="/" component={ Pokemons } exact/>
+                        <Route path="/pokemon-card" component={ PokemonCard }/>
+                        <Route component={ ErrorPage } />
+                    </Switch>
                 </Router>
               </div>
             </main>
