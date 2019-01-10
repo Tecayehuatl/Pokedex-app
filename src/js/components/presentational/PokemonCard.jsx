e829209a6c92c23214747381162cc2abb9d0827f
Enrique <enrique.delgado@live.com>
2019-01-10 03:06:30 -0600

    Update pokedex

--- src/js/components/presentational/PokemonCard.jsx
+++ src/js/components/presentational/PokemonCard.jsx
@@ -1,40 +1,37 @@
 import React, {Component} from 'react'
 //import {withRouter} from 'react-router-dom'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 class PokemonCard extends Component{
     constructor(props){
         super(props)
-        //console.log(this.props.location.state)
         this.state = {
             name: this.props.location.state.pokemonName.name,
-            url: this.props.location.state.pokemonName.url
+            url: this.props.location.state.pokemonName.url,
+            abilitiesLength: 0,
+            weight: 0,
+            types: []
         }
     }
     componentDidMount(){
         axios.get(this.state.url)
-            .then(response => {console.log(response.data)})
+            .then(response => {
+                console.log(response.data)
+                let data = response.data
+                this.setState({
+                    abilitiesLength: data.abilities.length || 0,
+                    weight: data.weight || 0,
+                    types: data.types
+                })
+            })
             .catch(response => {console.log(`Error ${response}`)})
     }
     render(){
+        
         return(
             <div>
-                
-                {/* <h6>{this.state.url}</h6> */}
-
-                {/* <div className="bs bs-point-right"></div>
-                <div className="bs bs-plus"></div>
-                <div className="bs bs-info"></div>
-                <div className="bs bs-home"></div>
-                 */}
-                
-                {/* <h1 className="card-title">{this.state.name}</h1>
-                <div className="card-body">
-                    <h5 className="c-card-title card-title"><span className="bs bs-pushpin"></span> - </h5>
-                    
-                    <p className="card-text">lorem.</p>
-
-                </div> */}
+                <Link className="c-link" to="/"><span className="bs bs-home"></span> Regresar</Link>
                 <div className="c-pokedex">
                     <div className="c-left-side">
                         <div className="c-left-header">
@@ -43,18 +40,29 @@ class PokemonCard extends Component{
                         </div>
                         <div className="c-left-body">
                             <div className="c-main-screen">
-                                <div className="c-main-screen__screen"></div>
-                                <div className="c-poke-button"></div>
+                                <div className="c-main-screen__screen">
+                                    <img className="c-main-screen__img" src="" />
+                                </div>
+                                <div className="c-poke-controls">
+                                    <div className="c-poke-button"></div>
+                                </div>
                             </div>
                         </div>
                         <div className="c-left-footer">
-                            <div className="c-poke-button"></div>
-                            <div className="c-poke-button"></div>
+                            <div className="c-footer-controls">
+                                <div className="c-poke-button c-poke-button--big c-poke-button--blue"></div>
+                                <div className="c-cross-button">
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    
                     <div className="c-right-side">
-                        Right side
+                        <div className="c-reader">
+                            <h2 className="u-capitalize"><span className="bs bs-info"></span> {this.state.name}</h2>
+                            <p>Número de habilidades de {this.state.name}: {this.state.abilitiesLength}</p>
+                            <p>Peso: {this.state.weight} Kg.</p>
+                            {/* <p>Tipos: { this.state.types.map(item => <span>{item}</span>)}</p> */}
+                        </div>
                     </div>
                 </div>
             </div>
